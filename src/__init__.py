1c257e16c6618c4cb53e92b752ad841012153dbd
Anuj Prakash <asprakas@iu.edu>
2026-04-25 23:21:04 -0400

    Delete src directory

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Elastic Distributed Video Processing Framework
-"""
-
-__version__ = "0.1.0"
-__authors__ = ["Anuj Prakash", "Arun Munagala", "Shreyas Amit Dhekane"]
--- src/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Core utilities for video processing framework
-"""
-
-from .config import Config
-from .logger import setup_logger
-
-__all__ = ['Config', 'setup_logger']
